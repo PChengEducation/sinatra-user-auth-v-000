5d564f8cde19679798661a17b84e363a84583b5f
Pan Cheng <pchengeducation@hotmail.com>
2018-08-25 20:54:24 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325184248) do
+ActiveRecord::Schema.define(version: 20180825205354) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
